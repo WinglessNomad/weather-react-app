749c5f84fb9b6d0391109b9e7e3edf13f9eef0a6
WinglessNomad <74137476+WinglessNomad@users.noreply.github.com>
2021-04-17 18:03:01 +0200

    Finished draft

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,23 +1,37 @@
 import React from "react";
 import Icon from "./Icon";
+import Date from "./Date";
+import "./CurrentWeather.css";
 
 export default function CurrentWeather(props) {
-    return (
-        <div>
-          <div>
-            <h1 className="city">{props.data.city}</h1>
-          </div>
-          <div>
-            <Icon code={props.data.icon} />
-            <p className="justLies">{props.data.description}</p>
-          </div>
-          
-          <ul className="weatherList">
-            <li className="temp">Temperature: {Math.round(props.data.temperature)} °C</li>
-            <li className="reality">Feels like: {Math.round(props.data.realFeel)} °C</li>
-            <li className="whoosh"> Wind speed: {Math.round(props.data.wind)} km/h </li>
-          </ul>
-          
-        </div>
-      );
+  return (
+    <div>
+      <div>
+        <h1 className="city">{props.data.city}</h1>
+      </div>
+      <div>
+        <Date date={props.data.date} />
+      </div>
+      <div className="icons">
+        <p className="justLies">{props.data.description}</p>
+        <Icon code={props.data.icon} />
+      </div>
+      <div className="temp">
+        {Math.round(props.data.temperature)}{" "}
+        <span className="celsius"> °C</span>
+      </div>
+      <div className="weatherList">
+        <ul>
+          <li className="reality">
+            {" "}
+            Feels like: {Math.round(props.data.realFeel)} °C{" "}
+          </li>
+          <li className="whoosh">
+            {" "}
+            Wind speed: {Math.round(props.data.wind)} km/h{" "}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,58 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
 import CurrentWeather from "./CurrentWeather";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
+import "./Weather.scss";
 
 export default function Weather(props) {
-    const [weather, setWeather] = useState({show: false});
-    const [city, setCity] = useState(props.default);
+  const [weather, setWeather] = useState({ show: false });
+  const [city, setCity] = useState(props.default);
 
-    function getResponses(response) {
-      setWeather({
-        show: true,
-        city: response.data.name,
-        temperature: response.data.main.temp,
-        description: response.data.weather[0].description,
-        realFeel: response.data.main.feels_like,
-        wind: response.data.wind.speed,
-        icon: response.data.weather[0].icon
-      });
-    }
+  function getResponses(response) {
+    setWeather({
+      show: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      realFeel: response.data.main.feels_like,
+      wind: response.data.wind.speed,
+      icon: response.data.weather[0].icon,
+    });
+  }
 
-    function showWeather() {
+  function showWeather() {
     let apiKey = "42e97aa1960c10d8826372a5ee85406c";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(getResponses);
-    }
+  }
 
-    function updateCity(event) {
+  function updateCity(event) {
     setCity(event.target.value);
-    }
+  }
 
-    function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
     showWeather();
-    }
+  }
 
-    if(weather.show) {
-      return (
+  if (weather.show) {
+    return (
+      <div className="container">
+        <div className="search_bar">
+          <form onSubmit={handleSubmit} className="form">
+            <input
+              type="search"
+              onChange={updateCity}
+              placeholder="What's the weather like in..."
+              className="searchBar"
+              aria-label="search"
+            />
+            <button
+              type="submit"
+              className="searchButton"
+              aria-label="submit search"
+            >
+              <FontAwesomeIcon icon={faSearchLocation} />
+            </button>
+          </form>
+        </div>
         <div>
-          <div>
-            <h2 className="title"> Weather App </h2>
-            <form onSubmit={handleSubmit} className="form">
-             <input
-                type="search"
-                onChange={updateCity}
-                placeholder="What's the weather like in..."
-                className="searchBar"
-             />
-             <input type="submit" value="Search" className="searchButton" />
-           </form>
-          </div>
-         <div> <CurrentWeather data={weather} /> </div>
-       </div>
-     );
-    } else {
-        showWeather();
-        return "Loading weather conditions...";
-        }
+          {" "}
+          <CurrentWeather data={weather} />{" "}
+        </div>
+      </div>
+    );
+  } else {
+    showWeather();
+    return "Loading weather conditions...";
+  }
 }
